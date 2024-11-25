e031188f380d25bde7eadd0b0d04047a28146975
Yash Sonar <sonaryash1406@gmail.com>
2024-11-25 12:10:36 +0530

    Security change

--- PDFinal/settings.py
+++ PDFinal/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "yourcityassist@gmail.com"
-EMAIL_HOST_PASSWORD = "ddgtdpwtvfvenpwx" # update the password 
+EMAIL_HOST_PASSWORD = "yourpassword" # update the password 
